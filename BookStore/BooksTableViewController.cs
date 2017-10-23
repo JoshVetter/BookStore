48b1f4aa2963b0922da5a8d201712377ab0bf22c
JoshVetter <32143305+JoshVetter@users.noreply.github.com>
2017-10-23 13:58:05 -0600

    added the lists

--- BookStore/BooksTableViewController.cs
+++ BookStore/BooksTableViewController.cs
@@ -1,13 +1,53 @@
 using Foundation;
 using System;
+using System.Collections.Generic;
+using System.CodeDom.Compiler;
 using UIKit;
 
 namespace BookStore
 {
     public partial class BooksTableViewController : UITableView
     {
-        public BooksTableViewController (IntPtr handle) : base (handle)
+        List<Book> booklist;
+        public BooksTableViewController(IntPtr handle) : base(handle)
         {
+            booklist = new List<Book>();
+
+            booklist.Add(new Book()
+            {
+                Author ="J.K. Rowling",
+                Name = "Harry Potter and the Sorcerer's Stone",
+                Publisher = "Bloomsbury",
+                Year = 1997
+            });
+			booklist.Add(new Book()
+			{
+				Author = "J.K. Rowling",
+				Name = "Harry Potter and the Chamber of Secrets",
+				Publisher = "Bloomsbury",
+				Year = 1998
+			});
+			booklist.Add(new Book()
+			{
+				Author = "J.K. Rowling",
+				Name = "Harry Potter and the Prisoner of Azkaban",
+				Publisher = "Bloomsbury",
+				Year = 1999
+			});
         }
+        public override nint NumberOfRowsInSection(nint section)
+        {
+            return 1;
+        }
+        public override nint
+
+    }
+
+    public class Book
+    {
+        public string Name;
+        public string Author;
+        public string Publisher;
+        public int Year;
     }
 }
