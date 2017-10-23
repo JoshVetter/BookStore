82534712ebab34f9bb03c8a36394dd52e90e8312
JoshVetter <32143305+JoshVetter@users.noreply.github.com>
2017-10-23 13:37:32 -0600

    Bookstore created

---
+++ BookStore/BooksTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BookStore
+{
+    public partial class BooksTableViewController : UITableView
+    {
+        public BooksTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ BookStore/TableView.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BookStore
+{
+    public partial class TableView : UITableViewController
+    {
+        public TableView (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
